c0e0f24a4f2f4202273ab465f37a960763827383
Alexandre <alexandre.lombard@live.fr>
2015-11-13 15:30:34 +0100

    Time evaluation in main function

--- src/main/java/test/spring/aop/Main.java
+++ src/main/java/test/spring/aop/Main.java
@@ -11,12 +11,14 @@ public class Main {
     public static void main(String[] args) {
         final Expression expr = new Plus(new Plus(new Number(5), new Number(7)), new Number(3));    // (5 + 7) + 3
 
-        long tic = System.nanoTime();
-        expr.eval();
-        System.out.println(System.nanoTime() - tic);
+        long time = 0;
 
-        tic = System.nanoTime();
-        expr.eval();
-        System.out.println(System.nanoTime() - tic);
+        for(int idx = 0; idx < 100; idx++) {
+            long tic = System.nanoTime();
+            expr.eval();
+            time += ((System.nanoTime() - tic) / 100);
+        }
+
+        System.out.println("Avg time: " + time);
     }
 }
